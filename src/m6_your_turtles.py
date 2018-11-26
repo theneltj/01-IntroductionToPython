a3b7bd5be071b78b00e95c6a3cec4f0c65756e44
theneltj <theneltj@rose-hulman.edu>
2018-11-26 11:22:31 -0500

    DONE

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Tyler Thenell.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,33 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+window.tracer(2)
+john = rg.SimpleTurtle("circle")
+john.pen = rg.Pen("white",0)
+john.forward(300)
+john.left(90)
+john.pen = rg.Pen('blue',3)
+john.forward(200)
+for k in range(54):
+    john.right(10)
+    john.forward(2)
+john.forward(200)
+john.backward(200)
+john.forward(200)
+
+tim = rg.SimpleTurtle("square")
+tim.speed = 500
+tim.pen = rg.Pen('red',3)
+tim.left(90)
+for i in range(615):
+    tim.forward(i)
+    tim.right(135)
+    if i > 200:
+        tim.pen = rg.Pen('dark orange', 3)
+    if i > 400:
+        tim.pen = rg.Pen('gold', 3)
+    if i > 600:
+        tim.pen = rg.Pen('black', 3)
+window.close_on_mouse_click()
